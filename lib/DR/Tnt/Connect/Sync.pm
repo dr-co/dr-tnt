8425114bf0a5670958ecb133bd77f9a396d2adf1
Dmitry E. Oboukhov <unera@debian.org>
2016-07-20 16:58:39 +0300

    Proto

--- lib/DR/Tnt/Connect/Sync.pm
+++ lib/DR/Tnt/Connect/Sync.pm
@@ -3,24 +3,57 @@ use strict;
 use warnings;
 
 package DR::Tnt::Connect::Sync;
-use DR::Tnt::Proto;
+use Mouse;
 use IO::Socket::INET;
 use IO::Socket::UNIX;
 use Carp;
 
+with 'DR::Tnt::Proto';
+
 sub connect {
     my ($class, %opts) = @_;
 
-    my ($host, $port, $login, $password) = DR::Tnt::Proto::dsn(%opts);
+    my ($host, $port, $login, $password) = $class->_dsn(%opts);
 
-
-    my $self = bless {
+    my $self = $class->new({
         host        => $host,
         port        => $port,
         login       => $login,
         password    => $password,
-    }   => ref($class) || $class;
+    });
+
+    $self;
 }
+
+
+
+
+sub _connect_reconnect {
+    my ($self) = @_;
+    my $fh;
+
+    if ($self->host eq 'unix/') {
+        $fh = IO::Socket::UNIX->new(
+
+        );
+    } else {
+        $fh = IO::Socket::INET->new(
+            PeerHost => $self->host,
+            PeerPort => $self->port,
+            Proto    => 'tcp',
+            (($^O eq 'MSWin32') ? () : (ReuseAddr => 1)),
+        );
+    }
+    $self->{fh} = $fh;
+}
+
+sub fh {
+    my ($self) = @_;
+    $self->_connect_reconnect unless $self->{fh};
+    $self->{fh};
+}
+
+
 
 
 1;
--- lib/DR/Tnt/Proto.pm
+++ lib/DR/Tnt/Proto.pm
@@ -4,31 +4,56 @@ use warnings;
 
 package DR::Tnt::Proto;
 use Carp;
+use MIME::Base64;
 
-sub dsn {
-    my %opts = @_;
-    my $host        = $opts{-host};
-    my $port        = $opts{-port};
+use constant TNT_CODE               => 0x00;
+use constant TNT_SYNC               => 0x01;
+use constant TNT_SCHEMA_ID          => 0x05;
+use constant TNT_SPACE_ID           => 0x10;
+use constant TNT_INDEX_ID           => 0x11;
+use constant TNT_LIMIT              => 0x12;
+use constant TNT_OFFSET             => 0x13;
+use constant TNT_ITERATOR           => 0x14;
+use constant TNT_KEY                => 0x20;
+use constant TNT_TUPLE              => 0x21;
+use constant TNT_FUNCTION           => 0x22;
+use constant TNT_USERNAME           => 0x23;
+use constant TNT_EXPRESSION         => 0x27;
+use constant TNT_OPS                => 0x28;
+use constant TNT_DATA               => 0x30;
+use constant TNT_ERROR              => 0x31;
+use constant TNT_CODE_SELECT        => 0x01;
+use constant TNT_CODE_INSERT        => 0x02;
+use constant TNT_CODE_REPLACE       => 0x03;
+use constant TNT_CODE_UPDATE        => 0x04;
+use constant TNT_CODE_DELETE        => 0x05;
+use constant TNT_CODE_CALL          => 0x06;
+use constant TNT_CODE_AUTH          => 0x07;
+use constant TNT_CODE_EVAL          => 0x08;
+use constant TNT_CODE_UPSERT        => 0x09;
+use constant TNT_CODE_PING          => 0x40;
+use constant TNT_RES_OK             => 0x00;
+use constant TNT_RES_ERROR          => 0x8000;
 
 
-    if ($port) {
-        if (!$host) {
-            if ($port =~ /^\d+$/) {
-                $host = '127.0.0.1';
-            } else {
-                $host = 'unix/';
-            }
-        }
-    } else {
-        croak "port is not defined";
+
+
+sub parse_greeting {
+    my ($str) = @_;
+    croak "strlen is not 128 bytes" unless $str and 128 == length $str;
+
+    my $salt = decode_base64(substr $str, 64, 44);
+    my $grstr = substr $str, 0, 64;
+
+    my ($title, $v, $pt, $uid) = split /\s+/, $grstr, 5;
+
+    return {
+        salt    => $salt,
+        gr      => $grstr,
+        title   => $title,
+        version => $v,
+        uuid    => $uid,
+        proto   => $pt,
     }
-
-
-    my $login       = $opts{-login}     || $opts{-user};
-    my $password    = $opts{-passwd}    || $opts{-password};
-
-    croak "login is not defined"    if $password    and !defined $login;
-    croak "password is not defined" if $login       and !defined $password;
-    return ($host, $port, $login, $password);
 }
 1;
