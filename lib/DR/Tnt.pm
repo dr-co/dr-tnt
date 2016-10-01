da77e98c9713fa54558fc2a4ec56f3fb874aa707
Dmitry E. Oboukhov <unera@debian.org>
2016-10-01 16:39:24 +0300

    Main module prototype

--- lib/DR/Tnt.pm
+++ lib/DR/Tnt.pm
@@ -3,16 +3,80 @@ use strict;
 use warnings;
 
 package DR::Tnt;
-use Mouse;
+use base qw(Exporter);
+our $VERSION = '0.01';
+our @EXPORT = qw(tarantool);
+use List::MoreUtils 'any';
 
-require DR::Tnt::LowLevel;
+use Carp;
+$Carp::Internal{ (__PACKAGE__) }++;
 
-use Mouse::Util::TypeConstraints;
+sub tarantool {
+    my (%opts) = @_;
 
-enum TntDrivers => [ 'sync', 'ae', 'coro' ];
+    my $driver = delete($opts{driver}) || 'sync';
+    
+    unless (any { $driver eq $_ } 'sync', 'ae', 'async', 'coro') {
+        goto usage;
+    }
+    goto $opts{driver};
 
-has driver      => is => 'ro', 
+
+    sync:
+        require DR::Tnt::Client::Sync;
+        return DR::Tnt::Client::Sync->new(%opts);
+
+    ae:
+    async:
+        require DR::Tnt::Client::AE;
+        return DR::Tnt::Client::AE->new(%opts);
+
+    coro:
+        require DR::Tnt::Client::Coro;
+        return DR::Tnt::Client::Coro->new(%opts);
 
 
+    usage:
 
-__PACKAGE__->meta->make_immutable;
+}
+
+
+=head1 NAME
+
+DR::Tnt - driver/connector for tarantool
+
+=head1 SYNOPSIS
+
+    use DR::Tnt;    # exports 'tarantool'
+
+    my $tnt = tarantool
+                    host                => '1.2.3.4',
+                    port                => 567,
+                    user                => 'my_tnt_user',
+                    password            => '#1@#$JHJH',
+                    hashify_tuples      => 1,
+                    driver              => 'sync',  # default
+                    lua_dir             => '/path/to/my/luas',
+                    reconnect_interval  => 0.5
+    ;
+
+    my $tuple = $tnt->get(space => 'index', [ 'key' ]);
+    my $tuples = $tnt->select(myspace => 'myindex', [ 'key' ], $limit, $offset);
+
+    my $updated = $tnt->update('myspace', [ 'key' ], [ [ '=', 1, 'name' ]]);
+    my $inserted = $tnt->insert(myspace => [ 1, 2, 3, 4 ]);
+    my $replaced = $tnt->replace(myspace => [ 1, 3, 4, 5 ]);
+
+    my $tuples = $tnt->call_lua('box.space.myspace:select', [ 'key' ]);
+    my $hashified_tuples =
+        $tnt->call_lua([ 'box.space.myspace:select' => 'myspace' ], ['key' ]);
+
+
+    my $removed = $tnt->delete(myspace => [ 'key' ]);
+   
+    my $tuples = $tnt->eval_lua('return 123');
+    my $hashify_tuples = $tnt->eval_lua(['return 123' => 'myspace' ]);
+
+
+=cut
+1;
